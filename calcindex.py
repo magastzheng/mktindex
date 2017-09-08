afac28a683955be6ba0b9b47044c60b17ffe4d6b
Zheng Guoqiang <zgq10722@htsec.com>
2017-09-08 17:40:09 +0800

    Add the code to calc flow captical

--- calcindex.py
+++ calcindex.py
@@ -40,12 +40,19 @@ def setShares(newdf, olddf):
 	df = pd.merge(newdf, tempdf, on='SecuCode', how='left')
 	return df
 	
-def getIndexPrice(df):
+def getIndexPrice(df, lblprice='ClosePrice', lblshare='Shares', lblmktcap='mktcap'):
+	"""	#获得价格总额
+		#df - 当期所有股票
+		#lblprice - 价格列名称
+		#lblshare - 股数列名称
+		#lblmktcap - 新列 市值列名称
+		#return - 价格总额
+	"""
 	#shares = df['Shares'].values.tolist()
 	#prices = df['ClosePrice'].values.tolist()
 	#df['mktcap'] = df.apply(lambda x: float(x['ClosePrice'])*x['Shares'], axis=1)
-	df.loc[:, 'mktcap'] = df.apply(lambda x: float(x['ClosePrice'])*x['Shares'], axis=1)
-	total = df['mktcap'].sum()
+	df.loc[:, lblmktcap] = df.apply(lambda x: float(x[lblprice])*x[lblshare], axis=1)
+	total = df[lblmktcap].sum()
 	return float(total)
 
 def equalWeightByTradingDay(df, mktcap=10000.0, base=1000):
@@ -69,12 +76,12 @@ def equalWeightByTradingDay(df, mktcap=10000.0, base=1000):
 		
 		if i == 0:
 			period = getShares(tempdf, mktcap)
-			pval = getIndexPrice(period)
+			pval = getIndexPrice(df=period, lblprice='ClosePrice', lblshare='Shares', lblmktcap='mktcap')
 			point = base
 			divisor = pval / base
 		else:
 			period = setShares(tempdf, predf)
-			pval = getIndexPrice(period)
+			pval = getIndexPrice(df=period, lblprice='ClosePrice', lblshare='Shares', lblmktcap='mktcap')
 			point = pval/divisor
 			
 			size = len(period)
@@ -109,18 +116,57 @@ def equalShareByTradingDay(df, shares=1000, base=1000):
 			period = tempdf
 			period.loc[:, "Shares"] = shares
 			#period = getShares(tempdf, mktcap)
-			pval = getIndexPrice(period)
+			pval = getIndexPrice(df=period, lblprice='ClosePrice', lblshare='Shares', lblmktcap='mktcap')
 			point = base
 			divisor = pval / base
 		else:
 			period = setShares(tempdf, predf)
-			pval = getIndexPrice(period)
+			pval = getIndexPrice(df=period, lblprice='ClosePrice', lblshare='Shares', lblmktcap='mktcap')
 			point = pval/divisor
 				
 			#重新分配股数-每支股票持有相同股数
 			totalprice = float(period['ClosePrice'].sum())
 			eachshare = pval / totalprice
 			period.loc[:, "Shares"] = eachshare
+			
+		predf = period
+		iv = IndexValue(td, pval, divisor, point)
+		idxvalues.append(iv)
+		
+	return idxvalues
+
+def mktcapByTradingDay(df, base=1000):	
+	#df - 月度数据
+	#base - 指数基点
+	tdarray = df['TradingDay'].unique()
+	tds = sorted(tdarray.tolist())
+	
+	idxvalues = []
+	predf = None
+	divisor = 0.0
+	point = 0.0
+	for i in range(len(tds)):
+		td = tds[i]
+		tempdf = df[df['TradingDay'] == td]
+		period = None
+		pval = 0.0
+		
+		if i == 0:
+			period = tempdf
+			period.loc[:, "Shares"] = period.loc[:, "AFloats"]
+			#period = getShares(tempdf, mktcap)
+			pval = getIndexPrice(df=period, lblprice='ClosePrice', lblshare='Shares', lblmktcap='mktcap')
+			point = base
+			divisor = pval / base
+		else:
+			period = setShares(tempdf, predf)
+			pval = getIndexPrice(df=period, lblprice='ClosePrice', lblshare='Shares', lblmktcap='mktcap')
+			point = pval/divisor
+				
+			#重新调整股数，使用流通股在平衡，此时需要调整除数divisor
+			#newpval = getIndexPrice(df=period, lblprice='ClosePrice', lblshare='AFloats', lblmktcap='nmktcap')
+			#divisor = newpval / point
+			#period.loc[:, "Shares"] = period.loc[:, "AFloats"]
 			
 		predf = period
 		iv = IndexValue(td, pval, divisor, point)
@@ -150,9 +196,16 @@ def draw(idxvalues):
 	
 if __name__ == "__main__":
 	#获得月度数据
-	df = getMonthlyData()
-	df.to_csv("./data/monthlydata.csv", encoding='utf8')
+	#df = getMonthlyData()
+	#df.to_csv("./data/monthlydata.csv", encoding='utf8')
+	df = pd.read_csv("./data/monthlydata.csv")
 	#计算指数
-	idxvalues = equalWeightByTradingDay(df)
+	#等权重
+	#idxvalues = equalWeightByTradingDay(df)
+	#等股份
+	#idxvalues = equalShareByTradingDay(df)
+	#等市值
+	idxvalues = mktcapByTradingDay(df)
+	print(idxvalues)
 	#画图
 	draw(idxvalues)
